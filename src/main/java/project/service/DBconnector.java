693be5c2f475c1ac24e501986f464b2d2da333ad
thorgeir93 <ths182@hi.is>
2016-04-17 23:36:50 +0000

    Database connection to heroku database

--- src/main/java/project/service/DBconnector.java
+++ src/main/java/project/service/DBconnector.java
@@ -46,7 +46,7 @@ public class DBconnector {
 			//this.connection = DriverManager.getConnection(
 			//		"jdbc:postgresql://localhost:5432/postgres", "postgres","1234");
 			this.connection = DriverManager.getConnection(
-					"jdbc:postgres://kakguotrzpwkpj:sGn4MZYX6CL1gJoNTUwLx1Q5Xz@ec2-54-225-111-9.compute-1.amazonaws.com:5432/d7ddgflvauj7k3", "kakguotrzpwkpj","sGn4MZYX6CL1gJoNTUwLx1Q5Xz");
+					"postgres://kakguotrzpwkpj:sGn4MZYX6CL1gJoNTUwLx1Q5Xz@ec2-54-225-111-9.compute-1.amazonaws.com:5432/d7ddgflvauj7k3", "kakguotrzpwkpj","sGn4MZYX6CL1gJoNTUwLx1Q5Xz");
 			
 
 		} catch (SQLException e) {
